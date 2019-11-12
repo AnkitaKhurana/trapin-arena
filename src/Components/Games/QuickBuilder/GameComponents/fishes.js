4a25c65615ddd49c1da01b7e5400bcfab94589ad
Ankita Khurana <Ankita.Khurana@netent.com>
2019-11-12 10:02:41 +0530

    Multiple Fishes

--- src/Components/Games/QuickBuilder/GameComponents/fishes.js
+++ src/Components/Games/QuickBuilder/GameComponents/fishes.js
@@ -2,10 +2,15 @@ import * as PIXI from "pixi.js";
 import {GameSpriteSheet} from "../../PixiUtilities/GameSpriteSheet.js";
 
 let fishes = (parentContainer) => {
-    let fishesContainer = new PIXI.Container();    
-    let purpleFishSprite = GameSpriteSheet("purpleRightFacingFish",1992,981,4,3,0.25, 0.5);
-    purpleFishSprite.play();
-    fishesContainer.addChild(purpleFishSprite);
+    let fishesContainer = new PIXI.Container();  
+    for(var i = 0 ; i < 5 ; i++)
+    {  
+        let purpleFishSprite = GameSpriteSheet("purpleRightFacingFish",1992,981,4,3,0.25, 0.5);
+        purpleFishSprite. x = 200*i;
+        purpleFishSprite.y = 400;
+        purpleFishSprite.play();
+        fishesContainer.addChild(purpleFishSprite);
+    }
     parentContainer.addChild(fishesContainer);
 }
 export let fishesSetup = (parentContainer) => {
